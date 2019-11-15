ea89b062e81062d1ed5b5d93671af270af97d988
jaeho214 <jaeho214@naver.com>
2019-11-15 22:02:45 +0900

    git init

---
+++ src/main/java/kr/ac/skuniv/medicalhelper/MedicalHelperApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.skuniv.medicalhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/skuniv/medicalhelper/MedicalHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.skuniv.medicalhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
