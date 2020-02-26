686ffa1bf34bfd74970c395ac65970460cf91e94
jaeho214 <jaeho214@naver.com>
2020-02-27 02:42:37 +0900

    update web config

--- src/main/java/kr/ac/skuniv/medicalhelper/global/config/WebConfig.java
+++ src/main/java/kr/ac/skuniv/medicalhelper/global/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.*;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("PUT", "DELETE", "GET", "POST")
                 .allowedHeaders("*")
